1657bef11d7bba670d44b854b880a36154b897ba
GabrielyMachado <gabriely.machado@hotmail.com>
2023-03-18 20:43:30 -0300

    feat(home): integra formulário com a lista de transações

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -4,6 +4,9 @@ class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
+  final void Function(String, double) onSubmit;
+  TransactionForm(this.onSubmit, {super.key});
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -29,8 +32,9 @@ class TransactionForm extends StatelessWidget {
               children: [
                 ElevatedButton(
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+                    onSubmit(title, value);
                   },
                   style: const ButtonStyle(
                       foregroundColor: MaterialStatePropertyAll(
