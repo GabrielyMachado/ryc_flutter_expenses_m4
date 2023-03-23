94461189298761169274672c73805087b6f37809
GabrielyMachado <gabriely.machado@hotmail.com>
2023-03-22 21:37:27 -0300

    feat(home): refatorando chamada do MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -91,8 +91,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       actions: [
@@ -111,9 +111,9 @@ class _MyHomePageState extends State<MyHomePage> {
       title: const Text('Despesas pessoais'),
     );
 
-    final availableHeight = MediaQuery.of(context).size.height -
+    final availableHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
-        MediaQuery.of(context).padding.top;
+        mediaQuery.padding.top;
     return Scaffold(
       appBar: appBar,
       body: Column(
